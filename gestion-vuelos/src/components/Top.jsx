8ca7300962ddda806a505f27896d4ab521ec394d
Jose <xjosex142@gmail.com>
2024-03-18 00:05:47 -0500

    primer intento

---
+++ gestion-vuelos/src/components/Top.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Top = () => {
+    <div className="main-container">
+        <div className="rectangle">
+          <div className="ellipse" />
+          <span className="singapur-airlines">Singapur Airlines</span>
+        </div>
+        <div className="flex-row">
+          <span className="no-vuelo">No Vuelo</span>
+          <span className="tipo-de-vuelo">Tipo de Vuelo</span>
+        </div>
+        <div className="flex-row-eb">
+          <div className="rectangle-1" />
+          <div className="rectangle-2" />
+        </div>
+        <div className="flex-row-ba">
+          <span className="ciudad-de-origen">Ciudad de Origen</span>
+          <span className="ciudad-de-destino">Ciudad de destino</span>
+        </div>
+        <div className="flex-row-ab">
+          <div className="rectangle-3" />
+          <div className="rectangle-4" />
+        </div>
+        <div className="flex-row-5">
+          <div className="button">
+            <span className="buscar">Buscar</span>
+          </div>
+          <div className="button-6">
+            <span className="crear-vuelo">Crear Vuelo</span>
+          </div>
+        </div>
+      </div>
+}
+
+export default Top;
